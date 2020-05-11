f080d93cbd109845fb573c5892c93c39a385fae2
Daniel Puth <dputh@student.ethz.ch>
2020-05-11 18:12:59 +0200

    Update chapter8.lua

--- Reminder/chapter8.lua
+++ Reminder/chapter8.lua
@@ -1,5 +1,10 @@
 local eR = elRaidoAddon
 eR.reminder = {}
+<<<<<<< Updated upstream
+=======
+eR.reminder.reminders = {}
+--eR.db.reminder = {}
+>>>>>>> Stashed changes
 local reminder = eR.reminder
 
 
@@ -10,16 +15,36 @@ reminder.bigwigs = function()
 	BigWigsLoader.RegisterMessage(plugin, "BigWigs_StartBar", startBarHandler)
 end
 
+<<<<<<< Updated upstream
+=======
+eR.reminder.remindersCLEU = { 
+							["SPELL_CAST_START"]   = {},
+	                        ["SPELL_CAST_SUCCESS"] = {},
+	                        ["SPELL_CAST_FAILED"]  = {},
+                            }
+setmetatable(eR.reminder.remindersCLEU, {__index=function(table, key) table[key]={}; return table[key] end})
+
+function reminder:addReminderCLEU(subevent, spellName, sourceName, destName)
+	local reminders = eR.reminder.remindersCLEU
+	reminders[subevent] = {spellName, sourceName, destName}
+end
+
+>>>>>>> Stashed changes
 reminder.eventFrame = CreateFrame("frame")
 reminder.eventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 reminder.eventFrame:SetScript("OnEvent", function(self, event) self:OnEvent(event, CombatLogGetCurrentEventInfo()) end)
 
 function reminder.eventFrame:OnEvent(event,...)
+<<<<<<< Updated upstream
 	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...	
+=======
+	local subevent = select(2,...)	
+>>>>>>> Stashed changes
 	if reminder.registeredSubevent[subevent] then reminder.handlerCLEU[subevent](...) end
 end
 
 reminder.handlerCLEU = {}
+<<<<<<< Updated upstream
 reminder.registeredSubevent = {}
 
 local handler = reminder.handlerCLEU
@@ -36,6 +61,28 @@ function(...)
 end
 local spellCastFailedHandler = 
 function(...) 
+=======
+reminder.registeredSubevent = {["SPELL_CAST_SUCCESS"]=true}
+
+local handler = reminder.handlerCLEU
+
+local spellCastStartHandler = function(...) 
+	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
+	local spellId, spellName, spellSchool = select(12, ...)
+	--stuff
+end
+
+local spellCastSuccessHandler = function(...) 
+	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
+	local spellId, spellName, spellSchool = select(12, ...)
+	local reminders = eR.reminder.remindersCLEU.SPELL_CAST_SUCCESS
+	if reminders[spellName] and reminders[spellName][sourceName] then print('activated') end
+	--stuff
+end
+
+local spellCastFailedHandler = function(...)
+	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
+>>>>>>> Stashed changes
 	local spellId, spellName, spellSchool = select(12, ...)
 	--stuff
 end
