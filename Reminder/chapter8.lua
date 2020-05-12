f5bd6e9d226e972573ce2ef5882bfa97f854797c
Daniel Puth <dputh@student.ethz.ch>
2020-05-12 02:14:46 +0200

    hopefully no weird shit

--- Reminder/chapter8.lua
+++ Reminder/chapter8.lua
@@ -1,12 +1,9 @@
 local eR = elRaidoAddon
 eR.reminder = {}
-<<<<<<< Updated upstream
-=======
+
 eR.reminder.reminders = {}
 --eR.db.reminder = {}
->>>>>>> Stashed changes
 local reminder = eR.reminder
-
 
 reminder.bigwigs = function()
 	local plugin = BigWigs:NewPlugin("elRaidoReminderBigWigs")
@@ -15,86 +12,79 @@ reminder.bigwigs = function()
 	BigWigsLoader.RegisterMessage(plugin, "BigWigs_StartBar", startBarHandler)
 end
 
-<<<<<<< Updated upstream
-=======
-eR.reminder.remindersCLEU = { 
-							["SPELL_CAST_START"]   = {},
-	                        ["SPELL_CAST_SUCCESS"] = {},
-	                        ["SPELL_CAST_FAILED"]  = {},
-                            }
+eR.reminder.remindersCLEU = {}
+reminder.registeredSubevent = {}
+
 setmetatable(eR.reminder.remindersCLEU, {__index=function(table, key) table[key]={}; return table[key] end})
 
-function reminder:addReminderCLEU(subevent, spellName, sourceName, destName)
+function reminder:addReminderCLEU(subevent, spellName, sourceName, destName, instances, encounter)
+	reminder.registeredSubevent[subevent] = true
 	local reminders = eR.reminder.remindersCLEU
-	reminders[subevent] = {spellName, sourceName, destName}
+	reminders[subevent][spellName..sourceName]=true   
+	reminders[subevent].instances = {}
+	reminders[subevent].counter = {}
+	reminders[subevent].instances[spellName..sourceName] = instances
+	reminders[subevent].counter[spellName..sourceName] = 0
 end
 
->>>>>>> Stashed changes
 reminder.eventFrame = CreateFrame("frame")
 reminder.eventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 reminder.eventFrame:SetScript("OnEvent", function(self, event) self:OnEvent(event, CombatLogGetCurrentEventInfo()) end)
 
 function reminder.eventFrame:OnEvent(event,...)
-<<<<<<< Updated upstream
 	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...	
-=======
 	local subevent = select(2,...)	
->>>>>>> Stashed changes
 	if reminder.registeredSubevent[subevent] then reminder.handlerCLEU[subevent](...) end
 end
 
 reminder.handlerCLEU = {}
-<<<<<<< Updated upstream
-reminder.registeredSubevent = {}
 
 local handler = reminder.handlerCLEU
 
-local spellCastStartHandler = 
-function(...) 
+handler["SPELL_CAST_START"] = function(...) 
+	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
 	local spellId, spellName, spellSchool = select(12, ...)
-	--stuff
+
+	local reminders = eR.reminder.remindersCLEU[subevent]
+	if reminders[spellName..sourceName] then 
+		reminders.counter[spellName..sourceName] = reminders.counter[spellName..sourceName] + 1
+		local counter = reminders.counter[spellName..sourceName] 
+		local instances = reminders.instances[spellName..sourceName] 
+		if instances == false or instances[counter] then print('actiaved'); end
+	end	
 end
-local spellCastSuccessHandler = 
-function(...) 
+
+handler["SPELL_CAST_SUCCESS"] = function(...) 
+
+	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
 	local spellId, spellName, spellSchool = select(12, ...)
-	--stuff
+
+	local reminders = eR.reminder.remindersCLEU[subevent]
+	if reminders[spellName..sourceName] then 
+		reminders.counter[spellName..sourceName] = reminders.counter[spellName..sourceName] + 1
+		local counter = reminders.counter[spellName..sourceName] 
+		local instances = reminders.instances[spellName..sourceName] 
+		if instances == false or instances[counter] then print('actiaved'); end
+	end
 end
-local spellCastFailedHandler = 
-function(...) 
-=======
-reminder.registeredSubevent = {["SPELL_CAST_SUCCESS"]=true}
 
-local handler = reminder.handlerCLEU
-
-local spellCastStartHandler = function(...) 
+handler["SPELL_CAST_FAILED"] = function(...)
 	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
 	local spellId, spellName, spellSchool = select(12, ...)
 	--stuff
+	local reminders = eR.reminder.remindersCLEU[subevent]
+	if reminders[spellName..sourceName] then 
+		reminders.counter[spellName..sourceName] = reminders.counter[spellName..sourceName] + 1
+		local counter = reminders.counter[spellName..sourceName] 
+		local instances = reminders.instances[spellName..sourceName] 
+		if instances == false or instances[counter] then print('actiaved'); end
+	end		
 end
-
-local spellCastSuccessHandler = function(...) 
-	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
-	local spellId, spellName, spellSchool = select(12, ...)
-	local reminders = eR.reminder.remindersCLEU.SPELL_CAST_SUCCESS
-	if reminders[spellName] and reminders[spellName][sourceName] then print('activated') end
-	--stuff
-end
-
-local spellCastFailedHandler = function(...)
-	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
->>>>>>> Stashed changes
-	local spellId, spellName, spellSchool = select(12, ...)
-	--stuff
-end
-
-handler["SPELL_CAST_START"]   = spellCastStartHandler
-handler["SPELL_CAST_SUCCESS"] = spellCastSuccessHandler
-handler["SPELL_CAST_FAILED"]  = spellCastFailedHandler
 
 local notTrackedHandler=
 function()
 	return
 end
 
-setmetatable(handler, {__index = function() return notTrackedHandler end})
-
+reminder:addReminderCLEU("SPELL_CAST_SUCCESS", "Thunder Clap", "Terriodasto", "", {[2]=true})
+reminder:addReminderCLEU("SPELL_CAST_START", "Hearthstone", "Terriodasto", "", {[2]=true})
--- Reminder/options.lua
+++ Reminder/options.lua
@@ -1,79 +1,34 @@
-local addon = OdastoAddonCLEU
-local addonName = OdastoAddonCLEU.name
+local eR = elRaidoAddon
+local reminder = eR.reminder
 local AceConfig=LibStub("AceConfig-3.0")
 local AceConfigDialog=LibStub("AceConfigDialog-3.0")
 
+function reminder:addReminder()
+    local args = reminder.optionsTable.args
+    local N = #args+1
+    args["reminder"..N] = {  name = 'reminder'..N, 
+                             type  = 'group', 
+                             args = { ["subevent"] = {name='subevent', type = 'select', values = {'chicken'}, order=1},
+                                      ["spellname"] = {name='spellName', type = 'input', order=2}
+                            }
+                          }
+end
 
-function addon:generateOptions()
-    addon.optionsTable = 
-    {	type='group',
+function reminder:generateOptions()
+    reminder.optionsTable = 
+    { type='group',
         --childGroups='tree',
         args={
         
        }
     }
     
-    
-    
-    AceConfig:RegisterOptionsTable('OdastoCLEU',addon.optionsTable)
-    AceConfigDialog:AddToBlizOptions('OdastoCLEU','OdastoCLEU')
-    addon:RegisterChatCommand("cleu", function(...) LibStub("AceConfigDialog-3.0"):Open('OdastoCLEU'); end)
+AceConfig:RegisterOptionsTable('elRaido',reminder.optionsTable)
+--AceConfigDialog:AddToBlizOptions('OdastoCLEU','OdastoCLEU')
+elRaidoAddon:RegisterChatCommand("cleu", function(...) LibStub("AceConfigDialog-3.0"):Open('elRaido'); end)
+
+
 end
 
-addon:generateOptions()
-
-local args = addon.optionsTable.args
-args["reminder1"] = {name='reminder1',
-              type='group',
-              order=1,
-              args={}
-              }
-args["reminder2"] = {name='reminder2',
-              type='group',
-              order=1,
-              args={}
-              }
-args["reminder3"] = {name='reminder3',
-              type='group',
-              order=1,
-              args={}
-              }              
-              
-args["general2"] = {name='Add Reminder',
-      type = "input",
-      set = function(info,val) end,
-      get = function(info) return end
-              
-              }              
-              
-              
-local args = addon.optionsTable.args.reminder1.args 
-
-args["who"] = {
-                name = 'who',
-                type = 'select',
-                set = function(info,val) end,
-                get = function(info) return end,
-                order=1,
-                values = {'by raider', 'by role', 'not vulperas'},
-}              
-
-
-args["what"] = {
-                name = 'what',
-                type = 'select',
-                set = function(info,val) end,
-                get = function(info) return 'scary mechanic' end,
-                order=2,
-                values = {'scary mechanic'},
-} 
-
-args["how"] = {
-                name = 'how',
-                type = 'select',
-                set = function(info,val) end,
-                get = function(info) return 'dancing chicken' end,
-                order=3,
-                values = {'dancing chicken'},
-} 
-
+reminder:generateOptions()
+reminder:addReminder()
